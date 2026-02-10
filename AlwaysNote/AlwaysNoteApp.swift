c572622c81f14e3c456b92a33f6b120a33f592b5
Daan Smit <daan.smit@kpnmail.nl>
2026-02-10 07:13:59 +0100

    Initial Commit

---
+++ AlwaysNote/AlwaysNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlwaysNoteApp.swift
+//  AlwaysNote
+//
+//  Created by DaanSmit on 10/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct AlwaysNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
